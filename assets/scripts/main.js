799600e324df41254a7b78b20d1c3010bd97c418
Nikhil Dange <nsdange@ucsd.edu>
2022-11-05 19:28:05 -0700

    most of expose

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -24,6 +24,12 @@ function getRecipesFromStorage() {
   // A9. TODO - Complete the functionality as described in this function
   //           header. It is possible in only a single line, but should
   //           be no more than a few lines.
+  let recipes = localStorage.getItem('recipies');
+
+  if (JSON.parse(recipes) === false){
+    return [];
+  }
+  return JSON.parse(recipes);
 }
 
 /**
